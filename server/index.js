a99ead2e1a2c37300b3b0fa181f8f29bcf3660d5
Keiran <keirankozlowski@gmail.com>
2018-11-26 23:59:55 -0500

    Added MongoDB

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+const MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://keiran:<Macduffer#1>@vcp-test-server-6xbdc.mongodb.net/test?retryWrites=true";
+MongoClient.connect(uri, function(err, db) {
+
+    db.close();
+});
+
 const server = express();
 server.use(express.json());
 server.use(helmet());
@@ -19,3 +26,5 @@ server.listen(port, () => console.log(`API running on port ${port}`));
 server.get('/', (request, response) => {
     response.send('Server initialized.');
 });
+
+
