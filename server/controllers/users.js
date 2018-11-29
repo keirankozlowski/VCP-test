1622b618c83cc2c7ef00e20eba988f1ca4510b32
Keiran <keirankozlowski@gmail.com>
2018-11-29 03:22:57 -0500

    Added some extra parameters to signup POST func

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,21 @@
+const User = require('../models/user');
+
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log('usersController.signUp called');
+        const { email, password } = req.value.body;
+
+        const foundUser = await User.findOne({ email });
+        if(foundUser) {
+            return res.status(409).send({ error: "Email is already used." });
+        }
+
+        const newUser = new User({
+            email,
+            password
+        });
+        await newUser.save();
+
+        res.json({ user: 'created' });
     },
 
     signIn: async (req, res, next) => {
