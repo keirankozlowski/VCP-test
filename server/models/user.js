111dfa1b0f88aad4e06c24a7bb260894dc4817de
Keiran <keirankozlowski@gmail.com>
2018-11-29 03:30:24 -0500

    Added some extra verification checks onto userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 const User = mongoose.model('user', userSchema);
